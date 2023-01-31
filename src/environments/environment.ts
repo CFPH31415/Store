921bf5088e10148dc1c2d45fd07f2ae32c546b27
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-31 18:19:32 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'proyecto29-4ef41',
+    appId: '1:110039823158:web:5837596ba12ff83695b997',
+    storageBucket: 'proyecto29-4ef41.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAMrS3gqn5SWHOfNLlK_QM73jnvVuXYUPU',
+    authDomain: 'proyecto29-4ef41.firebaseapp.com',
+    messagingSenderId: '110039823158',
+    measurementId: 'G-JRXSV2JS0F',
+  },
+}
